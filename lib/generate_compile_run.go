8171649415019c803bc6f1cfd32af62d31653e0c
Ernest Micklei <ernest.micklei@gmail.com>
2013-03-13 22:02:10 +0100

    got output capture of compiler and executable working :-)
    added comments, clean up stuff

---
+++ lib/generate_compile_run.go
@@ -0,0 +1,105 @@
+package lib
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"text/template"
+)
+
+var temporaryShellScriptName = "rangorun"
+
+// Generate_compile_run takes a list of Go source lines, create a Go program from it, compiles that source and runs that program.
+// Return the captured output from the compilation or the execution of the Go program.
+func Generate_compile_run(goSourceFile string, entries []SourceHolder) string {
+	err := generate(goSourceFile, entries)
+	if err != nil {
+		return fmt.Sprintf("[rango] generate Go source failed:%v", err)
+	}
+	output, _ := gorun(goSourceFile)
+	return output
+}
+
+// generate produces a Go source file from a list of Go code entries
+func generate(goSourceFile string, entries []SourceHolder) error {
+	t := template.Must(template.New("image").Parse(imageSourceTemplate()))
+	var sourceBuffer bytes.Buffer
+	t.Execute(&sourceBuffer, buildTemplateVars(entries))
+	return ioutil.WriteFile(goSourceFile, sourceBuffer.Bytes(), 0644)
+}
+
+// gorun compiles and executes the source found in goSourceFile and reports its output
+// The output can be compiler or runtime error info, otherwise the output is what the program spits out.
+func gorun(goSourceFile string) (string, error) {
+	logName := fmt.Sprintf("%s.build.log", goSourceFile)
+
+	// In order to capture the output of "go run", a temporary script is generated and executed by the shell
+	// Note: using the script contents in a Command, Run/Start it while capturing Stdout & Stdout via Pipes does not work.
+	script := fmt.Sprintf("go run %s > %s  2>&1", goSourceFile, logName)
+	buf := new(bytes.Buffer)
+	buf.WriteString(script)
+	ioutil.WriteFile(temporaryShellScriptName, buf.Bytes(), os.ModePerm)
+	// clean up afterwards
+	defer os.Remove(temporaryShellScriptName)
+
+	cmd := exec.Command("sh", temporaryShellScriptName)
+	runError := cmd.Run()
+
+	// The captured output has been written to a temporary log file
+	file, err := os.Open(logName)
+	if err != nil {
+		return "[rango] open output failed", err
+	}
+	// make sure it is closed before return (and removal)
+	defer file.Close()
+	// clean up afterwards
+	defer os.Remove(logName)
+
+	logBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		return "[rango] read output failed", err
+	}
+	return string(logBytes), runError
+}
+
+// buildTemplateVars creates a templateVars struct from the list of code entries
+func buildTemplateVars(entries []SourceHolder) templateVars {
+	imageVars := new(templateVars)
+	for i, each := range entries {
+		switch each.Type {
+		case Import:
+			imageVars.Imports = append(imageVars.Imports, each.Source)
+		case Print:
+			// only preserve the last
+			if i == len(entries)-1 {
+				imageVars.Statements = append(imageVars.Statements, each.Source)
+			}
+		default:
+			imageVars.Statements = append(imageVars.Statements, each.Source)
+		}
+	}
+	return *imageVars
+}
+
+// templateVars holds the template variables for the Go source to evaluate
+type templateVars struct {
+	Imports    []string
+	Statements []string
+}
+
+// imageSourceTemplate returns a Go program template that requires templateVars to produce Go source
+func imageSourceTemplate() string {
+	return `package main
+import "fmt"
+{{range .Imports}}{{.}}
+{{end}}
+func nop(v interface{}){}
+func main() {
+fmt.Print("")
+{{range .Statements}}{{.}}
+{{end}}
+}
+`
+}
--- lib/sourceholder.go
+++ lib/sourceholder.go
@@ -12,28 +12,33 @@ const (
 	Print
 )
 
+// SourceHolder is basically one line of Go source code with meta data
 type SourceHolder struct {
-	LoopCount int
-	Type      int
-	Source    string
-	Hidden    bool
+	LoopCount int    // In which REPL count was this created
+	Type      int    // one of the constants Import,...
+	Source    string // Go code entered or hidden code produced by rango
+	Hidden    bool   // If false then hide this from source listing
 	// type data
-	ImportNames   []string
-	VariableNames []string
+	ImportNames   []string // If the holder is of type Import then store the packages here
+	VariableNames []string // If the holder is of type VariableDecl then store the variable names here
 }
 
+// Hide marks a SourceHolder as a hidden line ; they will not show up in source listing
 func (s *SourceHolder) Hide() {
 	s.Hidden = true
 }
 
+// NewImport creates a new SourceHolder of type Import
 func NewImport(loopcount int, source string) SourceHolder {
 	return SourceHolder{LoopCount: loopcount, Type: Import, Source: source}
 }
 
+// NewStatement creates a new SourceHolder of type Statement
 func NewStatement(loopcount int, source string) SourceHolder {
 	return SourceHolder{LoopCount: loopcount, Type: Statement, Source: source}
 }
 
+// NewVariableDecl creates a new SourceHolder of type VariableDecl
 func NewVariableDecl(loopcount int, source string) SourceHolder {
 	return SourceHolder{
 		LoopCount:     loopcount,
@@ -42,10 +47,13 @@ func NewVariableDecl(loopcount int, source string) SourceHolder {
 		VariableNames: ParseVariableNames(source)}
 }
 
+// NewPrint creates a new SourceHolder of type Print
 func NewPrint(loopcount int, source string) SourceHolder {
 	return SourceHolder{LoopCount: loopcount, Type: Print, Source: source, Hidden: true}
 }
 
+// AppendTo adds a new SourceHolder to the collection of entries
+// As a side effect, it may produce additional SourceHolders based on its type.
 func (s SourceHolder) AppendTo(holders []SourceHolder) []SourceHolder {
 	extended := append(holders, s)
 	if VariableDecl == s.Type {
@@ -57,10 +65,12 @@ func (s SourceHolder) AppendTo(holders []SourceHolder) []SourceHolder {
 	return extended
 }
 
+// IsVariable says whether the receiver is known as declared Variable name.
 func (s SourceHolder) IsVariable(entry string) bool {
 	return s.Type == VariableDecl && s.VariableNames[0] == entry
 }
 
+// IsVariableDeclaration says whether the receiver is of such type.
 func IsVariableDeclaration(source string) bool {
 	assignmentIndex := strings.Index(source, ":=")
 	return assignmentIndex > 0
@@ -73,6 +83,7 @@ func ParseVariableNames(source string) []string {
 	return []string{varName}
 }
 
+// CollectVariables returns the list of declared variable names entered by the user.
 func CollectVariables(holders []SourceHolder) []string {
 	names := []string{}
 	for _, each := range holders {
